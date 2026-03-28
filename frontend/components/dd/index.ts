2984255733ec137fd84ab2839e616edab4158f7c
akmalss19800606-web <akmalss19800606@gmail.com>
2026-03-28 21:21:04 +0500

    feat(dd): add barrel export index.ts

---
+++ frontend/components/dd/index.ts
@@ -0,0 +1,8 @@
+// Barrel export for DD components
+export { default as CompanyProfileCard } from './CompanyProfileCard';
+export { default as DueDiligenceLayout } from './DueDiligenceLayout';
+export { ScoreBadge, RiskBadge } from './ScoreBadge';
+export { default as FinancialHighlights } from './FinancialHighlights';
+
+// Re-export types
+export type { DDTab } from './DueDiligenceLayout';
